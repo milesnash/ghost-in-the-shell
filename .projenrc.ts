e804a6c0a5ba74cce8c70c41ac9d06095f99ba99
Miles <miles.nash37@gmail.com>
2023-02-16 23:28:46 +0000

    Add automerge flow

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,4 +1,4 @@
-import { javascript, typescript } from "projen";
+import { github, javascript, typescript } from "projen";
 
 const tsconfig = {
   compilerOptions: {
@@ -33,12 +33,48 @@ const project = new typescript.TypeScriptAppProject({
   },
 });
 
+// NPM scripts
 project.tasks
   .tryFind("package")
   ?.prependExec("webpack --mode production -o docs");
 
 project.addTask("start", {
   exec: "webpack && webpack-dev-server --mode development",
+});
+
+// GitHub Actions
+const githubComponent = github.GitHub.of(project)!;
+
+const autoMergeWorkflow = githubComponent.addWorkflow("automerge");
+
+autoMergeWorkflow.on({
+  pullRequest: {
+    types: ["labeled", "opened", "ready_for_review", "reopened", "synchronize"],
+  },
+  checkSuite: {
+    types: ["completed"],
+  },
+  status: {},
+});
+
+autoMergeWorkflow.addJobs({
+  automerge: {
+    if: "github.actor == github.repository_owner",
+    permissions: {
+      pullRequests: github.workflows.JobPermission.WRITE,
+    },
+    runsOn: ["ubuntu-latest"],
+    steps: [
+      {
+        uses: "peter-evans/enable-pull-request-automerge@v2",
+        with: {
+          token: "${{ secrets.PROJEN_GITHUB_TOKEN }}",
+          "pull-request-number": "${{ github.event.pull_request.number }}",
+          "merge-method": "squash",
+        },
+      },
+    ],
+  },
 });
 
 project.synth();
